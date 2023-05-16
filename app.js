c68c31f145ae81e72e0cb166679a37cedb943077
Ahmed Fayed <90969100+X-Fayed@users.noreply.github.com>
2023-05-16 23:31:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const dbConnection = require("./config/db");
 const app = express();
 app.use(express.json({ extended: true }));
@@ -18,6 +19,7 @@ const port = 5000 ;
 
 //***public middelware */
 app.use(express.json());
+app.use(cors());
 
 dbConnection();
 
